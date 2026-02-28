1b5ed4861a3ec9b9c54b933768ac0fb4c3abe017
user47638828 <he72nkhe5biw7@gmail.com>
2026-02-28 01:00:50 +0100

    Delete js directory

--- js/backend.js
+++
@@ -1,9 +0,0 @@
-const Backend = {
-  allowedSeedHashes: [
-    "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
-  ],
-
-  async verifySeed(hash) {
-    return this.allowedSeedHashes.includes(hash);
-  }
-};
--- js/cryptography.js
+++
@@ -1,7 +0,0 @@
-async function hashSHA256(payload) {
-  const data = new TextEncoder().encode(payload);
-  const digest = await crypto.subtle.digest("SHA-256", data);
-  return Array.from(new Uint8Array(digest))
-    .map(b => b.toString(16).padStart(2, "0"))
-    .join("");
-}
