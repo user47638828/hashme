8467a3ff65e433f27b26b04112af7a9fad5ffb4f
user47638828 <he72nkhe5biw7@gmail.com>
2026-02-28 01:43:00 +0100

    Update fz4fu6e4u7rerzu8.js

--- fz4fu6e4u7rerzu8.js
+++ fz4fu6e4u7rerzu8.js
@@ -1,10 +1,14 @@
-const F={apiKey:"DEIN_KEY",projectId:"DEIN_PROJEKT"};
-firebase.initializeApp(F);
+firebase.initializeApp({
+  apiKey:"AIzaSyDhF-31FZGTuNhQdI7UUQqG5mKRDe4VWLc",
+  projectId:"hashme-dev-fbe21"
+});
 const D=firebase.firestore();
-const E=t=>new TextEncoder().encode(t);
-const H=async v=>{
- const r=await crypto.subtle.digest("SHA-256",E(v));
- return Array.from(new Uint8Array(r)).map(b=>b.toString(16).padStart(2,"0")).join("");
-};
+
 const S=(k,v)=>localStorage.setItem(k,v);
 const G=k=>localStorage.getItem(k);
+
+const H=async v=>{
+  const e=new TextEncoder().encode(v);
+  const r=await crypto.subtle.digest("SHA-256",e);
+  return Array.from(new Uint8Array(r)).map(b=>b.toString(16).padStart(2,"0")).join("");
+};
